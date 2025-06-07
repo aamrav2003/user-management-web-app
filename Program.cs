19b0bd14d60e0357daaf17a90b498b407edf1b46
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:44:17 +1000

    Add Program.cs 1749278656396

---
+++ Program.cs
@@ -0,0 +1,78 @@
+using FleetProLayered_Architecture.DataAccess.Repositories;
+using IAB_251_Assessment2.Application.Interfaces;
+using IAB_251_Assessment2.Application.Services;
+using IAB_251_Assessment2.BusinessLogic.Interfaces;
+using IAB_251_Assessment2.BusinessLogic.Services;
+using IAB_251_Assessment2.DataAccess.Data;
+using IAB_251_Assessment2.DataAccess.Interfaces;
+using IAB_251_Assessment2.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UserManagementDBContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+
+
+builder.Services.AddScoped<IUserRegistrationAppService, UserRegistrationAppService>();
+builder.Services.AddScoped<IUserRegistrationService, UserRegistrationService>();
+builder.Services.AddSingleton<IUserRepository, InMemoryUserDB>();
+
+builder.Services.AddSingleton<IUserRepository, InMemoryUserDB>();
+builder.Services.AddTransient<IUserRegistrationService, UserRegistrationService>();
+
+builder.Services.AddScoped<IUserRepository, EFUserRepository>();
+builder.Services.AddScoped<IUserRegistrationService, UserRegistrationService>();
+builder.Services.AddScoped<IUserRegistrationAppService, UserRegistrationAppService>();
+
+builder.Services.AddScoped<IQuoteRepository, EFQuoteRepository>();
+builder.Services.AddScoped<IQuotationService, QuotationService>();
+builder.Services.AddScoped<IQuoteAppService, QuoteAppService>();    
+
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication("MyCookieAuth")
+    .AddCookie("MyCookieAuth", options =>
+    {
+        options.LoginPath = "/Users/UserLogin";
+        options.AccessDeniedPath = "/AccessDenied";
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+//app.UseHttpsRedirection();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapStaticAssets();
+//app.MapRazorPages()
+//   .WithStaticAssets();
+
+//app.Run();
