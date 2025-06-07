112754d49c0c6554edd57ceeae2ab6cfad89e304
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:44:35 +1000

    Add Pages/Users/Models/UserViewModel.cs 1749278674558

---
+++ Pages/Users/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace IAB_251_Assessment2.Pages.User.Model
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNumber { get; set; }
+        public string CompanyName { get; set; } //only if applicable -> optional??
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
