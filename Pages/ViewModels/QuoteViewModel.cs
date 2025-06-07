f844ace81fb8617de3a9e510f830e4b698b16554
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:44:24 +1000

    Add Pages/ViewModels/QuoteViewModel.cs 1749278663495

---
+++ Pages/ViewModels/QuoteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IAB_251_Assessment2.Pages.ViewModels
+{
+    public class QuoteViewModel
+    {
+        public int Id { get; set; }
+        public string clientName { get; set; }
+        public DateTime dateIssued { get; set; }
+        public string status { get; set; }
+
+        //–– I4/I6 fields ––
+        public decimal Rate { get; set; }
+        public decimal Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string CustomerDecision { get; set; }
+    }
+}
