2053f25e4237240bd33d19b12c0d8ff08e257b6c
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:21 +1000

    Add Migrations/20250506141722_AddQuotationTable.cs 1749278720881

---
+++ Migrations/20250506141722_AddQuotationTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IAB_251_Assessment2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuotationTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quotations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    clientName = table.Column<string>(type: "TEXT", nullable: false),
+                    dateIssued = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    status = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quotations", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quotations");
+        }
+    }
+}
