6011c68b895216a69271edf1a8763c6e903fe013
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:43 +1000

    Add Application/Interfaces/IUserRegistrationAppService.cs 1749278743075

---
+++ Application/Interfaces/IUserRegistrationAppService.cs
@@ -0,0 +1,18 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+
+namespace IAB_251_Assessment2.Application.Interfaces
+{
+    public interface IUserRegistrationAppService
+    {
+        List<User> GetAllUsers();
+        User GetUser(int id);
+
+        void AddUser(User user);
+
+        void UpdateUser(User user);
+
+        void DeleteUser(int id);
+
+
+    }
+}
