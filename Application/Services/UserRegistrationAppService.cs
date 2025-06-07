6e0d13752bf82cfefb41280d2d7ea01a063532ba
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:34 +1000

    Add Application/Services/UserRegistrationAppService.cs 1749278733866

---
+++ Application/Services/UserRegistrationAppService.cs
@@ -0,0 +1,27 @@
+﻿using IAB_251_Assessment2.Application.Interfaces;
+using IAB_251_Assessment2.BusinessLogic.Entities;
+using IAB_251_Assessment2.BusinessLogic.Interfaces;
+
+namespace IAB_251_Assessment2.Application.Services
+{
+    public class UserRegistrationAppService : IUserRegistrationAppService
+    {
+
+        private readonly IUserRegistrationService _userService;
+
+        public UserRegistrationAppService(IUserRegistrationService userService)
+        {
+            _userService = userService;
+        }
+
+        public List<User> GetAllUsers() => _userService.GetAll();
+
+        public User GetUser(int id) => _userService.GetById(id);
+
+        public void AddUser(User user) => _userService.Add(user);
+
+        public void UpdateUser(User user) => _userService.Update(user);
+
+        public void DeleteUser(int id) => _userService.Delete(id);
+    }
+}
