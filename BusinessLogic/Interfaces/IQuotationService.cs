85a9573d4f2ee0dd0190c38b59a21f14f59266c4
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:46 +1000

    Add BusinessLogic/Interfaces/IQuotationService.cs 1749278745701

---
+++ BusinessLogic/Interfaces/IQuotationService.cs
@@ -0,0 +1,14 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+
+namespace IAB_251_Assessment2.BusinessLogic.Interfaces
+{
+    public interface IQuotationService
+    {
+
+        List<Quotation> GetAll();
+        Quotation GetById(int id);
+        void Add(Quotation quote);
+        void Update(Quotation quote);
+        void Delete(int id);
+    }
+}
