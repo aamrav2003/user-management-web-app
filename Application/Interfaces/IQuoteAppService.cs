96c3c02b7cc70b30473370e4ea36c70c29aa5510
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:50 +1000

    Add Application/Interfaces/IQuoteAppService.cs 1749278749635

---
+++ Application/Interfaces/IQuoteAppService.cs
@@ -0,0 +1,13 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+
+namespace IAB_251_Assessment2.Application.Interfaces
+{
+    public interface IQuoteAppService
+    {
+        List<Quotation> GetAllQuotes();
+        Quotation GetQuote(int id);
+        void AddQuote(Quotation quote);
+        void UpdateQuote(Quotation quote);
+        void DeleteQuote(int id);
+    }
+}
