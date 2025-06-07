d65744c970fc5071da5da1651811c4aed05a5e00
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:45 +1000

    Add BusinessLogic/Interfaces/IUserRegistrationService.cs 1749278744408

---
+++ BusinessLogic/Interfaces/IUserRegistrationService.cs
@@ -0,0 +1,17 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+
+namespace IAB_251_Assessment2.BusinessLogic.Interfaces
+{
+    public interface IUserRegistrationService
+    {
+
+        List<User> GetAll();
+        User GetById(int id);
+
+        void Add(User user);
+
+        void Update(User user);
+
+        void Delete(int id);
+    }
+}
