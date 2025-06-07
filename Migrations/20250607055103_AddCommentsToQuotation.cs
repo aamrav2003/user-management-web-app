9266b1e25df9b715a5893392c39f9de6839d44df
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:10 +1000

    Add Migrations/20250607055103_AddCommentsToQuotation.cs 1749278709378

---
+++ Migrations/20250607055103_AddCommentsToQuotation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IAB_251_Assessment2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCommentsToQuotation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "Quotations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CustomerResponseDate",
+                table: "Quotations",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreparedBy",
+                table: "Quotations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Quotations");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerResponseDate",
+                table: "Quotations");
+
+            migrationBuilder.DropColumn(
+                name: "PreparedBy",
+                table: "Quotations");
+        }
+    }
+}
