84a09fa5f8c2ece98d8e87c97aa6748d1392a8d0
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:44:25 +1000

    Add Pages/Users/UserViewDetails.cshtml.cs 1749278664982

---
+++ Pages/Users/UserViewDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IAB_251_Assessment2.Pages.Users
+{
+    public class UserViewDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
