5a2e7505357f1b504eb8e3d8d52291c850291473
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:24 +1000

    Add DataAccess/Interfaces/IQuoteRepository.cs 1749278724013

---
+++ DataAccess/Interfaces/IQuoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+namespace IAB_251_Assessment2.DataAccess.Interfaces
+{
+    public interface IQuoteRepository
+    {
+        List<Quotation> GetAll();
+        Quotation GetById(int id);
+        void Add(Quotation user);
+        void Update(Quotation user);
+        void Delete(int id);
+    }
+}
