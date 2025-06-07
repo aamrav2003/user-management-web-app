89d9866c83e70fa9f15b91e0f6d197bc7230327b
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:30 +1000

    Add Application/Services/QuoteAppService.cs 1749278729722

---
+++ Application/Services/QuoteAppService.cs
@@ -0,0 +1,26 @@
+﻿using IAB_251_Assessment2.Application.Interfaces;
+using IAB_251_Assessment2.BusinessLogic.Entities;
+using IAB_251_Assessment2.BusinessLogic.Interfaces;
+
+namespace IAB_251_Assessment2.Application.Services
+{
+    public class QuoteAppService: IQuoteAppService
+    {
+        private readonly IQuotationService _quoteService;
+
+        public QuoteAppService(IQuotationService quoteService)
+        {
+            _quoteService = quoteService;
+        }
+
+        public List<Quotation> GetAllQuotes() => _quoteService.GetAll();
+
+        public Quotation GetQuote(int id) => _quoteService.GetById(id);
+
+        public void AddQuote(Quotation quote) => _quoteService.Add(quote);
+
+        public void UpdateQuote(Quotation quote) => _quoteService.Update(quote);
+
+        public void DeleteQuote(int id) => _quoteService.Delete(id);
+    }
+}
