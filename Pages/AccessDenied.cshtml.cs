068a0d6b3b996abbdc017b59bbf17b0e1d1d18a4
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:44:18 +1000

    Add Pages/AccessDenied.cshtml.cs 1749278657759

---
+++ Pages/AccessDenied.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IAB_251_Assessment2.Pages
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public AccessDeniedModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
