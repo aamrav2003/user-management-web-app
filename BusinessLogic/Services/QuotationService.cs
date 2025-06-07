caf283d33c6fbfa9716a10d0b4d35307dd994ce4
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:36 +1000

    Add BusinessLogic/Services/QuotationService.cs 1749278735254

---
+++ BusinessLogic/Services/QuotationService.cs
@@ -0,0 +1,26 @@
+﻿using IAB_251_Assessment2.BusinessLogic.Entities;
+using IAB_251_Assessment2.BusinessLogic.Interfaces;
+using IAB_251_Assessment2.DataAccess.Interfaces;
+
+namespace IAB_251_Assessment2.BusinessLogic.Services
+{
+    public class QuotationService: IQuotationService
+    {
+        private readonly IQuoteRepository _quoteRepository;
+
+        public QuotationService(IQuoteRepository quoteRepository)
+        {
+            _quoteRepository = quoteRepository;
+        }
+
+        public List<Quotation> GetAll() => _quoteRepository.GetAll();
+
+        public Quotation GetById(int id) => _quoteRepository.GetById(id);
+
+        public void Add(Quotation quote) => _quoteRepository.Add(quote);
+
+        public void Update(Quotation quote) => _quoteRepository.Update(quote);
+
+        public void Delete(int id) => _quoteRepository.Delete(id);
+    }
+}
