4564aed3788d35f56a7363eb2444ae82e9b4f6dc
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 17:34:06 +1000

    Add BusinessLogic/Entities/User.cs 1749281645605

---
+++ BusinessLogic/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿﻿namespace IAB_251_Assessment2.BusinessLogic.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string FamilyName { get; set; }
+        public string EmailAddress { get; set; }
+        public int PhoneNumber { get; set; }
+        public string CompanyName { get; set; } //only if applicable -> optional??
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } // Added Role property to distinguish employee/customer
+    }
+}
