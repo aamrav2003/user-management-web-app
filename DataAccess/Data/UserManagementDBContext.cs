af595deae44fc766a096b5fb95682f34f32c76cf
aamrav2003 <97047868+aamrav2003@users.noreply.github.com>
2025-06-07 16:45:37 +1000

    Add DataAccess/Data/UserManagementDBContext.cs 1749278736690

---
+++ DataAccess/Data/UserManagementDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IAB_251_Assessment2.BusinessLogic.Entities;
+using System.Collections.Generic;
+
+namespace IAB_251_Assessment2.DataAccess.Data
+{
+    public class UserManagementDBContext : DbContext
+    {
+        public UserManagementDBContext(DbContextOptions<UserManagementDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Quotation> Quoations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<Quotation>().ToTable("Quotations");
+        }
+    }
+}
